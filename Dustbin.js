d7f28817a9f18ffc7df895a1e499b8352310c430
Craze02 <70636961+Craze02@users.noreply.github.com>
2020-12-07 15:45:21 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,32 @@
+class Dustbin {
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic: true,
+            restitution: 0.8,
+            density: 1.0,
+            friction: 1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("dustbingreen.png");
+        World.add(world, this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        push();
+        fill("red");
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+    }
+
+    addPic() {
+        var pos = this.body.position;
+        push();
+        imageMode(CENTER);
+        image(this.image, 600, 600, this.width, this.height);
+        pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload() {
+
+}
+
+function setup() {
+	createCanvas(1200, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(600, height, 1200, 20);
+	paper = new Paper(100, 200, 50);
+	rightRect = new Dustbin(685, 600, 10, 50);
+	leftRect = new Dustbin(515, 600, 10, 50);
+	baseRect = new Dustbin(0, 0, 200, 200);
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+	rectMode(CENTER);
+	background("white");
+	rightRect.display();
+	leftRect.display();
+	//baseRect.display();
+	ground.display();
+	paper.display();
+	baseRect.addPic();
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paper.body, paper.body.position, { x: 300, y: -300 });
+	}
+}
